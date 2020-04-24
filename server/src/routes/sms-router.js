aee36c5b99f9396abe01c447d30d38d476db571d
leonardo.silva <leonardo.silva@terceiros.zenvia.com>
2020-04-24 10:26:32 -0300

    Removing node_modules folder and add gitignore

--- server/src/routes/sms-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const express = require("express");
-const router = express.Router();
-const controller = require('../controller/sms-controller');
-
-router.post('/v1/callbackBroker/', controller.post);
-
-module.exports = router;
